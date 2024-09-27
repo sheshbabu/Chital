c46786339a0d41bd3ef61abeee582975bc8483de
Sheshbabu <sheshbabu@gmail.com>
2024-09-27 23:24:14 +0800

    Turned off in-memory store

--- Chital/ChitalApp.swift
+++ Chital/ChitalApp.swift
@@ -8,7 +8,7 @@ struct ChitalApp: App {
             ChatThread.self,
             ChatMessage.self
         ])
-        let configuration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: true)
+        let configuration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
         
         do {
             return try ModelContainer(for: schema, configurations: [configuration])
