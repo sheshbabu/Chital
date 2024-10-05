1a337c44fd1d0255aca95e12454bb943f7f4661d
Sheshbabu <sheshbabu@gmail.com>
2024-10-05 10:38:19 +0800

    Moved copy button to bottom of chat bubble

--- Chital/ChatBubbleView.swift
+++ Chital/ChatBubbleView.swift
@@ -12,26 +12,42 @@ struct ChatBubbleView: View {
                     .font(.caption)
                     .foregroundColor(.secondary)
                     .padding(.vertical, 2)
-                
-                Button(action: copyText) {
-                    Image(systemName: "doc.on.doc")
-                }
-                .help("Copy")
-                .buttonStyle(PlainButtonStyle())
-                .font(.caption)
-                .foregroundColor(.secondary)
-                .opacity(isHovering ? 1 : 0)
-                .offset(x: isHovering ? 0 : -10)
-                .animation(.easeInOut(duration: 0.2), value: isHovering)
-                
             }
             
-            Markdown(message.text)
-                .padding(12)
-                .textSelection(.enabled)
-                .background(message.isUser ? Color.accentColor.opacity(0.2) : Color(NSColor.textBackgroundColor))
-                .cornerRadius(8)
-                .frame(maxWidth: .infinity, alignment: .leading)
+            ZStack(alignment: .bottom) {
+                Markdown(message.text)
+                    .padding(12)
+                    .textSelection(.enabled)
+                    .background(message.isUser ? Color.accentColor.opacity(0.2) : Color(NSColor.textBackgroundColor))
+                    .cornerRadius(8)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                
+                HStack() {
+                    Button(action: copyText) {
+                        Label("Copy", systemImage: "doc.on.doc")
+                    }
+                    .help("Copy")
+                    .buttonStyle(PlainButtonStyle())
+                    .font(.caption)
+                    .foregroundColor(.secondary)
+                    .frame(alignment: .leading)
+                    .padding(.vertical, 4)
+                    .padding(.horizontal, 8)
+                    .background(
+                        RoundedRectangle(cornerRadius: 6, style: .continuous)
+                            .fill(Color(NSColor.textBackgroundColor))
+                    )
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 6)
+                            .stroke(Color.gray.opacity(0.3), lineWidth: 0.5)
+                    )
+                    .offset(y: 16)
+                    .opacity(isHovering ? 1 : 0)
+                    .animation(.easeInOut(duration: 0.2), value: isHovering)
+                    
+                    Spacer()
+                }
+            }
         }
         .onHover { hovering in
             isHovering = hovering
