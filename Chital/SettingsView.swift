36e2d00c80227c4e072f9d56b5140aec29fd715b
Sheshbabu <sheshbabu@gmail.com>
2024-10-07 19:28:19 +0800

    Added reset of default model name

--- Chital/SettingsView.swift
+++ Chital/SettingsView.swift
@@ -69,6 +69,7 @@ struct SettingsView: View {
                     titleSummaryPrompt = AppConstants.titleSummaryPrompt
                     contextWindowLength = AppConstants.contextWindowLength
                     contextWindowLengthString = "\(AppConstants.contextWindowLength)"
+                    defaultModelName = AppConstants.defaultModelName
                 }
             }
             .padding()
